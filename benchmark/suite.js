482d6a4e90924e520a29f2d6a884a672e72190e5
Phil Pluckthun <phil@kitten.sh>
2023-03-20 04:53:24 +0000

    Add initial benchmark suite

---
+++ benchmark/suite.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const graphqlWeb = require('..');
+const graphql = require('graphql');
+
+const kitchenSink = fs.readFileSync('./kitchen_sink.graphql', { encoding: 'utf8' });
+const document = graphql.parse(kitchenSink, { noLocation: true });
+
+suite('parse kitchen sink query', () => {
+  benchmark('0no-co/graphql.web', () => {
+    graphqlWeb.parse(kitchenSink);
+  });
+
+  benchmark('graphql', () => {
+    graphql.parse(kitchenSink, { noLocation: true });
+  });
+});
+
+suite('print kitchen sink query', () => {
+  benchmark('0no-co/graphql.web', () => {
+    graphqlWeb.print(document);
+  });
+
+  benchmark('graphql', () => {
+    graphql.print(document);
+  });
+});
