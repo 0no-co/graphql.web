a7ac3de7ce9e7d6826fbada1d64564c78537b2f4
Phil Pluckthun <phil@kitten.sh>
2023-03-21 09:05:45 +0000

    Reformat parse tests

--- src/__tests__/parser.test.ts
+++ src/__tests__/parser.test.ts
@@ -5,8 +5,8 @@ import { parse as graphql_parse } from 'graphql';
 import { parse, parseType, parseValue } from '../parser';
 import { Kind } from '../kind';
 
-describe('print', () => {
-  it('prints the kitchen sink document like graphql.js does', () => {
+describe('parse', () => {
+  it('parses the kitchen sink document like graphql.js does', () => {
     const sink = readFileSync(__dirname + '/../../benchmark/kitchen_sink.graphql', {
       encoding: 'utf8',
     });
@@ -235,199 +235,216 @@ describe('print', () => {
     const result = parse('{ id }', { noLocation: true });
     expect('loc' in result).toBe(false);
   });
+});
 
-  describe('parseValue', () => {
-    it('parses null value', () => {
-      const result = parseValue('null');
-      expect(result).toEqual({ kind: Kind.NULL });
-    });
+describe('parseValue', () => {
+  it('parses null value', () => {
+    const result = parseValue('null');
+    expect(result).toEqual({ kind: Kind.NULL });
+  });
 
-    it('parses list values', () => {
-      const result = parseValue('[123 "abc"]');
-      expect(result).toEqual({
-        kind: Kind.LIST,
-        values: [
-          {
-            kind: Kind.INT,
-            value: '123',
-          },
-          {
-            kind: Kind.STRING,
-            value: 'abc',
-            block: false,
-          },
-        ],
-      });
-    });
-
-    it('parses integers', () => {
-      expect(parseValue('12')).toEqual({
-        kind: Kind.INT,
-        value: '12',
-      });
-
-      expect(parseValue('-12')).toEqual({
-        kind: Kind.INT,
-        value: '-12',
-      });
-    });
-
-    it('parses floats', () => {
-      expect(parseValue('12e2')).toEqual({
-        kind: Kind.FLOAT,
-        value: '12e2',
-      });
-
-      expect(parseValue('0.2E3')).toEqual({
-        kind: Kind.FLOAT,
-        value: '0.2E3',
-      });
-
-      expect(parseValue('-1.2e+3')).toEqual({
-        kind: Kind.FLOAT,
-        value: '-1.2e+3',
-      });
-    });
-
-    it('parses strings', () => {
-      expect(parseValue('"test"')).toEqual({
-        kind: Kind.STRING,
-        value: 'test',
-        block: false,
-      });
-
-      expect(parseValue('"\\t\\t"')).toEqual({
-        kind: Kind.STRING,
-        value: '\t\t',
-        block: false,
-      });
-    });
-
-    it('parses objects', () => {
-      expect(parseValue('{}')).toEqual({
-        kind: Kind.OBJECT,
-        fields: [],
-      });
-
-      expect(() => parseValue('{name}')).toThrow();
-      expect(() => parseValue('{name:}')).toThrow();
-      expect(() => parseValue('{name:null')).toThrow();
-
-      expect(parseValue('{name:null}')).toEqual({
-        kind: Kind.OBJECT,
-        fields: [{
-          kind: Kind.OBJECT_FIELD,
-          name: {
-            kind: Kind.NAME,
-            value: 'name'
-          },
-          value: {
-            kind: Kind.NULL,
-          },
-        }],
-      });
-    });
-
-    it('parses block strings', () => {
-      expect(parseValue('["""long""" "short"]')).toEqual({
-        kind: Kind.LIST,
-        values: [
-          {
-            kind: Kind.STRING,
-            value: 'long',
-            block: true,
-          },
-          {
-            kind: Kind.STRING,
-            value: 'short',
-            block: false,
-          },
-        ],
-      });
-
-      expect(parseValue('"""\n\n  first\n  second\n"""')).toEqual({
-        kind: Kind.STRING,
-        value: 'first\nsecond',
-        block: true,
-      });
-    });
-
-    it('allows variables', () => {
-      const result = parseValue('{ field: $var }');
-      expect(result).toEqual({
-        kind: Kind.OBJECT,
-        fields: [
-          {
-            kind: Kind.OBJECT_FIELD,
-            name: {
-              kind: Kind.NAME,
-              value: 'field',
-            },
-            value: {
-              kind: Kind.VARIABLE,
-              name: {
-                kind: Kind.NAME,
-                value: 'var',
-              },
-            },
-          },
-        ],
-      });
-    });
-
-    it('correct message for incomplete variable', () => {
-      expect(() => {
-        return parseValue('$');
-      }).toThrow();
-    });
-
-    it('correct message for unexpected token', () => {
-      expect(() => {
-        return parseValue(':');
-      }).toThrow();
+  it('parses list values', () => {
+    const result = parseValue('[123 "abc"]');
+    expect(result).toEqual({
+      kind: Kind.LIST,
+      values: [
+        {
+          kind: Kind.INT,
+          value: '123',
+        },
+        {
+          kind: Kind.STRING,
+          value: 'abc',
+          block: false,
+        },
+      ],
     });
   });
 
-  describe('parseType', () => {
-    it('parses well known types', () => {
-      const result = parseType('String');
-      expect(result).toEqual({
-        kind: Kind.NAMED_TYPE,
-        name: {
-          kind: Kind.NAME,
-          value: 'String',
-        },
-      });
+  it('parses integers', () => {
+    expect(parseValue('12')).toEqual({
+      kind: Kind.INT,
+      value: '12',
     });
 
-    it('parses custom types', () => {
-      const result = parseType('MyType');
-      expect(result).toEqual({
+    expect(parseValue('-12')).toEqual({
+      kind: Kind.INT,
+      value: '-12',
+    });
+  });
+
+  it('parses floats', () => {
+    expect(parseValue('12e2')).toEqual({
+      kind: Kind.FLOAT,
+      value: '12e2',
+    });
+
+    expect(parseValue('0.2E3')).toEqual({
+      kind: Kind.FLOAT,
+      value: '0.2E3',
+    });
+
+    expect(parseValue('-1.2e+3')).toEqual({
+      kind: Kind.FLOAT,
+      value: '-1.2e+3',
+    });
+  });
+
+  it('parses strings', () => {
+    expect(parseValue('"test"')).toEqual({
+      kind: Kind.STRING,
+      value: 'test',
+      block: false,
+    });
+
+    expect(parseValue('"\\t\\t"')).toEqual({
+      kind: Kind.STRING,
+      value: '\t\t',
+      block: false,
+    });
+  });
+
+  it('parses objects', () => {
+    expect(parseValue('{}')).toEqual({
+      kind: Kind.OBJECT,
+      fields: [],
+    });
+
+    expect(() => parseValue('{name}')).toThrow();
+    expect(() => parseValue('{name:}')).toThrow();
+    expect(() => parseValue('{name:null')).toThrow();
+
+    expect(parseValue('{name:null}')).toEqual({
+      kind: Kind.OBJECT,
+      fields: [{
+        kind: Kind.OBJECT_FIELD,
+        name: {
+          kind: Kind.NAME,
+          value: 'name'
+        },
+        value: {
+          kind: Kind.NULL,
+        },
+      }],
+    });
+  });
+
+  it('parses block strings', () => {
+    expect(parseValue('["""long""" "short"]')).toEqual({
+      kind: Kind.LIST,
+      values: [
+        {
+          kind: Kind.STRING,
+          value: 'long',
+          block: true,
+        },
+        {
+          kind: Kind.STRING,
+          value: 'short',
+          block: false,
+        },
+      ],
+    });
+
+    expect(parseValue('"""\n\n  first\n  second\n"""')).toEqual({
+      kind: Kind.STRING,
+      value: 'first\nsecond',
+      block: true,
+    });
+  });
+
+  it('allows variables', () => {
+    const result = parseValue('{ field: $var }');
+    expect(result).toEqual({
+      kind: Kind.OBJECT,
+      fields: [
+        {
+          kind: Kind.OBJECT_FIELD,
+          name: {
+            kind: Kind.NAME,
+            value: 'field',
+          },
+          value: {
+            kind: Kind.VARIABLE,
+            name: {
+              kind: Kind.NAME,
+              value: 'var',
+            },
+          },
+        },
+      ],
+    });
+  });
+
+  it('correct message for incomplete variable', () => {
+    expect(() => {
+      return parseValue('$');
+    }).toThrow();
+  });
+
+  it('correct message for unexpected token', () => {
+    expect(() => {
+      return parseValue(':');
+    }).toThrow();
+  });
+});
+
+describe('parseType', () => {
+  it('parses well known types', () => {
+    const result = parseType('String');
+    expect(result).toEqual({
+      kind: Kind.NAMED_TYPE,
+      name: {
+        kind: Kind.NAME,
+        value: 'String',
+      },
+    });
+  });
+
+  it('parses custom types', () => {
+    const result = parseType('MyType');
+    expect(result).toEqual({
+      kind: Kind.NAMED_TYPE,
+      name: {
+        kind: Kind.NAME,
+        value: 'MyType',
+      },
+    });
+  });
+
+  it('parses list types', () => {
+    const result = parseType('[MyType]');
+    expect(result).toEqual({
+      kind: Kind.LIST_TYPE,
+      type: {
         kind: Kind.NAMED_TYPE,
         name: {
           kind: Kind.NAME,
           value: 'MyType',
         },
-      });
+      },
     });
+  });
 
-    it('parses list types', () => {
-      const result = parseType('[MyType]');
-      expect(result).toEqual({
-        kind: Kind.LIST_TYPE,
-        type: {
-          kind: Kind.NAMED_TYPE,
-          name: {
-            kind: Kind.NAME,
-            value: 'MyType',
-          },
+  it('parses non-null types', () => {
+    const result = parseType('MyType!');
+    expect(result).toEqual({
+      kind: Kind.NON_NULL_TYPE,
+      type: {
+        kind: Kind.NAMED_TYPE,
+        name: {
+          kind: Kind.NAME,
+          value: 'MyType',
         },
-      });
+      },
     });
+  });
 
-    it('parses non-null types', () => {
-      const result = parseType('MyType!');
-      expect(result).toEqual({
+  it('parses nested types', () => {
+    const result = parseType('[MyType!]');
+    expect(result).toEqual({
+      kind: Kind.LIST_TYPE,
+      type: {
         kind: Kind.NON_NULL_TYPE,
         type: {
           kind: Kind.NAMED_TYPE,
@@ -436,24 +453,7 @@ describe('print', () => {
             value: 'MyType',
           },
         },
-      });
-    });
-
-    it('parses nested types', () => {
-      const result = parseType('[MyType!]');
-      expect(result).toEqual({
-        kind: Kind.LIST_TYPE,
-        type: {
-          kind: Kind.NON_NULL_TYPE,
-          type: {
-            kind: Kind.NAMED_TYPE,
-            name: {
-              kind: Kind.NAME,
-              value: 'MyType',
-            },
-          },
-        },
-      });
+      },
     });
   });
-});
+})
