79c7af530768f8ec664c5188612e57ec4c752262
Phil Pluckthun <phil@kitten.sh>
2023-03-21 08:36:21 +0000

    Set up coverage provider

---
+++ vitest.config.ts
@@ -0,0 +1,14 @@
+import { resolve } from 'path';
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    coverage: {
+      enabled: true,
+      provider: 'c8',
+      100: true,
+    },
+    globals: false,
+    clearMocks: true,
+  },
+});
