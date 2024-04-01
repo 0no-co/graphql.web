828e7083f0a40c1cb201abe6a294ec20b88d5e02
Phil Pluckthun <phil@kitten.sh>
2024-04-01 23:38:42 +0100

    Remove name() helper

--- src/parser.ts
+++ src/parser.ts
@@ -66,15 +66,6 @@ function ignored() {
 }
 
 const nameRe = /[_A-Za-z]\w*/y;
-function name(): ast.NameNode | undefined {
-  let match: string | undefined;
-  if ((match = advance(nameRe))) {
-    return {
-      kind: 'Name' as Kind.NAME,
-      value: match,
-    };
-  }
-}
 
 const valueRe = new RegExp(
   '(?:' +
@@ -111,7 +102,7 @@ function value(constant: true): ast.ConstValueNode;
 function value(constant: boolean): ast.ValueNode;
 
 function value(constant: boolean): ast.ValueNode {
-  let match: string | ast.NameNode | undefined;
+  let match: string | undefined;
   let exec: ValueExec | null;
   valueRe.lastIndex = idx;
   if (input.charCodeAt(idx) === 91 /*'['*/) {
@@ -130,13 +121,13 @@ function value(constant: boolean): ast.ValueNode {
     ignored();
     const fields: ast.ObjectFieldNode[] = [];
     while (input.charCodeAt(idx) !== 125 /*'}'*/) {
-      if ((match = name()) == null) throw error('ObjectField');
+      if ((match = advance(nameRe)) == null) throw error('ObjectField');
       ignored();
       if (input.charCodeAt(idx++) !== 58 /*':'*/) throw error('ObjectField');
       ignored();
       fields.push({
         kind: 'ObjectField' as Kind.OBJECT_FIELD,
-        name: match,
+        name: { kind: 'Name' as Kind.NAME, value: match },
         value: value(constant),
       });
     }
@@ -210,17 +201,17 @@ function arguments_(constant: boolean): ast.ArgumentNode[] | undefined {
     const args: ast.ArgumentNode[] = [];
     idx++;
     ignored();
-    let _name: ast.NameNode | undefined;
+    let _name: string | undefined;
     let _value: ast.ValueNode | undefined;
     do {
-      if ((_name = name()) == null) throw error('Argument');
+      if ((_name = advance(nameRe)) == null) throw error('Argument');
       ignored();
       if (input.charCodeAt(idx++) !== 58 /*':'*/) throw error('Argument');
       ignored();
       if ((_value = value(constant)) == null) throw error('Argument');
       args.push({
         kind: 'Argument' as Kind.ARGUMENT,
-        name: _name,
+        name: { kind: 'Name' as Kind.NAME, value: _name },
         value: _value,
       });
     } while (input.charCodeAt(idx) !== 41 /*')'*/);
@@ -237,14 +228,14 @@ function directives(constant: boolean): ast.DirectiveNode[] | undefined {
   ignored();
   if (input.charCodeAt(idx) === 64 /*'@'*/) {
     const directives: ast.DirectiveNode[] = [];
+    let _name: string | undefined;
     do {
       idx++;
-      const _name = name();
-      if (!_name) throw error('Directive');
+      if ((_name = advance(nameRe)) == null) throw error('Directive');
       ignored();
       directives.push({
         kind: 'Directive' as Kind.DIRECTIVE,
-        name: _name,
+        name: { kind: 'Name' as Kind.NAME, value: _name },
         arguments: arguments_(constant),
       });
     } while (input.charCodeAt(idx) === 64 /*'@'*/);
@@ -253,7 +244,7 @@ function directives(constant: boolean): ast.DirectiveNode[] | undefined {
 }
 
 function type(): ast.TypeNode {
-  let match: ast.NameNode | ast.TypeNode | undefined;
+  let match: string | ast.TypeNode | undefined;
   ignored();
   if (input.charCodeAt(idx) === 91 /*'['*/) {
     idx++;
@@ -263,10 +254,10 @@ function type(): ast.TypeNode {
       kind: 'ListType' as Kind.LIST_TYPE,
       type: match,
     };
-  } else if ((match = name())) {
+  } else if ((match = advance(nameRe)) != null) {
     match = {
       kind: 'NamedType' as Kind.NAMED_TYPE,
-      name: match,
+      name: { kind: 'Name' as Kind.NAME, value: match },
     };
   } else {
     throw error('NamedType');
@@ -343,18 +334,13 @@ function selectionSet(): ast.SelectionSetNode {
           });
         }
       } else if ((match = exec[SelectionGroup.Name]) != null) {
-        let _alias: ast.NameNode | undefined;
-        let _name: ast.NameNode | undefined = {
-          kind: 'Name' as Kind.NAME,
-          value: match,
-        };
+        let _alias: string | undefined;
         ignored();
         if (input.charCodeAt(idx) === 58 /*':'*/) {
           idx++;
           ignored();
-          _alias = _name;
-          if ((_name = name()) == null) throw error('Field');
-          ignored();
+          _alias = match;
+          if ((match = advance(nameRe)) == null) throw error('Field');
         }
         const _arguments = arguments_(false);
         const _directives = directives(false);
@@ -366,8 +352,8 @@ function selectionSet(): ast.SelectionSetNode {
         }
         selections.push({
           kind: 'Field' as Kind.FIELD,
-          alias: _alias,
-          name: _name,
+          alias: _alias ? { kind: 'Name' as Kind.NAME, value: _alias } : undefined,
+          name: { kind: 'Name' as Kind.NAME, value: match },
           arguments: _arguments,
           directives: _directives,
           selectionSet: _selectionSet,
@@ -391,10 +377,10 @@ function variableDefinitions(): ast.VariableDefinitionNode[] | undefined {
     const vars: ast.VariableDefinitionNode[] = [];
     idx++;
     ignored();
-    let _name: ast.NameNode | undefined;
+    let _name: string | undefined;
     do {
       if (input.charCodeAt(idx++) !== 36 /*'$'*/) throw error('Variable');
-      if ((_name = name()) == null) throw error('Variable');
+      if ((_name = advance(nameRe)) == null) throw error('Variable');
       ignored();
       if (input.charCodeAt(idx++) !== 58 /*':'*/) throw error('VariableDefinition');
       const _type = type();
@@ -409,7 +395,7 @@ function variableDefinitions(): ast.VariableDefinitionNode[] | undefined {
         kind: 'VariableDefinition' as Kind.VARIABLE_DEFINITION,
         variable: {
           kind: 'Variable' as Kind.VARIABLE,
-          name: _name,
+          name: { kind: 'Name' as Kind.NAME, value: _name },
         },
         type: _type,
         defaultValue: _defaultValue,
@@ -450,12 +436,12 @@ const definitionRe = /(?:query|mutation|subscription|fragment)/y;
 function operationDefinition(
   operation: OperationTypeNode | undefined
 ): ast.OperationDefinitionNode | undefined {
-  let _name: ast.NameNode | undefined;
+  let _name: string | undefined;
   let _variableDefinitions: ast.VariableDefinitionNode[] | undefined;
   let _directives: ast.DirectiveNode[] | undefined;
   if (operation) {
     ignored();
-    _name = name();
+    _name = advance(nameRe);
     _variableDefinitions = variableDefinitions();
     _directives = directives(false);
   }
@@ -465,7 +451,7 @@ function operationDefinition(
     return {
       kind: 'OperationDefinition' as Kind.OPERATION_DEFINITION,
       operation: operation || ('query' as OperationTypeNode.QUERY),
-      name: _name,
+      name: _name ? { kind: 'Name' as Kind.NAME, value: _name } : undefined,
       variableDefinitions: _variableDefinitions,
       directives: _directives,
       selectionSet: selectionSet(),
