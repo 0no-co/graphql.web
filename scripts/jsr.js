b283574bc23bffadfb2c016d81c56503a621f82c
jdecroock <decroockjovi@gmail.com>
2024-04-24 19:04:23 +0200

    add experimental

--- scripts/jsr.js
+++ scripts/jsr.js
@@ -7,6 +7,6 @@ const jsrJsonSource = path.resolve(__dirname, '../jsr.json');
 const packageJson = JSON.parse(fs.readFileSync(packageJsonSource, { encoding: 'utf-8' }));
 const jsrJson = JSON.parse(fs.readFileSync(jsrJsonSource, { encoding: 'utf-8' }));
 
-jsrJson.version = packageJson.version;
+jsrJson.version = packageJson.version + '-experimental.0';
 
 fs.writeFileSync(jsrJsonSource, JSON.stringify(jsrJson, undefined, 2));
