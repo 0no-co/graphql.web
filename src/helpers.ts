99b72ebdddda3382b29b53145318779a15444708
jdecroock <decroockjovi@gmail.com>
2024-10-31 11:29:47 +0100

    Fix slow type

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,11 +1,11 @@
-import type { Location } from './types';
+import type { Location, Source as _Source } from './types';
 import type { ASTNode, SelectionNode } from './ast';
 
 export function isSelectionNode(node: ASTNode): node is SelectionNode {
   return node.kind === 'Field' || node.kind === 'FragmentSpread' || node.kind === 'InlineFragment';
 }
 
-export function Source(body: string, name?: string, locationOffset?: Location) {
+export function Source(body: string, name?: string, locationOffset?: Location): _Source {
   return {
     body,
     name,
